ec60f47cd27fd8b562b37c00067d804e0b3a1001
EfimovSchool <sergio-251@yandex.ru>
2022-01-05 23:18:14 +0300

    Final setup

--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = "Start"
+    
+    @State private var nextColor: Color = .red
+    
+    @State private var redColor = LightView(color: .red)
+    @State private var yellowColor = LightView(color: .yellow)
+    @State private var greenColor = LightView(color: .green)
+    
+    private var button: some View {
+        Button {
+            isPressed()
+        } label: {
+            Text(name)
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width: 150, height: 40)
+                .background(.blue)
+                .cornerRadius(20)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                redColor
+                yellowColor
+                greenColor
+                Spacer()
+                button
+            }
             .padding()
+        }
+    }
+    
+    private func isPressed() {
+        name = "Next"
+        switch nextColor {
+        case .red:
+            nextColor = .yellow
+            redColor.isOn = true
+            greenColor.isOn = false
+        case .yellow:
+            nextColor = .green
+            yellowColor.isOn = true
+            redColor.isOn = false
+        default:
+            nextColor = .red
+            greenColor.isOn = true
+            yellowColor.isOn = false
+        }
     }
 }
 
---
+++ TrafficLightsSwiftUI/LightView.swift
@@ -0,0 +1,32 @@
+//
+//  LightView.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Sergey Efimov on 05.01.2022.
+//
+
+import SwiftUI
+
+struct LightView: View {
+    let color: Color
+    var isOn: Bool = false
+    
+    private var opacity: Double {
+        isOn ? 1.0 : 0.3
+    }
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .opacity(opacity)
+            .frame(width: 150, height: 150)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            
+    }
+}
+
+struct LightView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightView(color: .red)
+    }
+}
